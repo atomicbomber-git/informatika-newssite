5de1e3bc024d85f96198260e2d4e03fe248c9f40
James Patrick Keegan <atomicbomber.email@gmail.com>
2019-04-27 12:06:40 +0700

    Added responsive voice support.

--- resources/views/guest_artikel/show.blade.php
+++ resources/views/guest_artikel/show.blade.php
@@ -14,7 +14,7 @@
         <img src="{{ route("artikel.main_image", $artikel) }}" alt="{{ $artikel->judul }}">
     </div>
 
-    <div class="ui segment">
+    <div class="ui segment" id="artikel">
         <div class="m-t:5">
             {!! $artikel->isi !!}
         </div>
@@ -22,3 +22,43 @@
 </div>
 
 @endsection
+
+@push("extra-scripts")
+<script src="//code.responsivevoice.org/responsivevoice.js?key=0beyBZkI"></script>
+
+<script>
+
+$(document).ready(function() {
+    $("#artikel p").each(function (index, elem) {
+        let button = $(`
+            <div class='t-a:r m-b:2'>
+                <button class='ui icon button red'>
+                    <i class='icon play'></i>
+                </button>
+            </div>
+        `)
+
+        $(button).click(function() {
+            let text = $(elem).text()
+
+            $.post("http://nlp-service.democlient.club/", { text: text })
+                .done(response => {
+                    responsiveVoice.speak(response.data.text, "Indonesian Female");
+                })
+                .fail((xhr, status, error) => {
+                    let response = JSON.parse(xhr.responseText);
+                });
+        })
+
+        let container = $(`
+            <div> </div>
+        `)
+
+        $(container).insertAfter(elem)
+        $(elem).appendTo(container)
+        $(button).insertAfter(elem)
+    })
+})
+
+</script>
+@endpush
